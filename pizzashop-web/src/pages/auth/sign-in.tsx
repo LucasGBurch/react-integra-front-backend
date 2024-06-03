b54c5653131fd99c9e52baba391d28b081fb2bb5
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2024-06-03 10:57:26 -0300

    Página: Cadastro de restaurante
     Aula para criar página sign-up

--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,7 +1,8 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -37,13 +38,19 @@ export function SignIn() {
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant={"ghost"} asChild className="absolute right-4 top-8">
+          <Link to="/sign-up">
+            Novo estabelecimento
+          </Link>
+        </Button>
+
         <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
             </h1>
             <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas pelo painel do parceiro
+              Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
 
