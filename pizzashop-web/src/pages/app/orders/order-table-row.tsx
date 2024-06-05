ae790061669ade7300ee9ba3d85a6be44bd7b733
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2024-06-05 11:47:41 -0300

    Componente: detalhes do pedido
    Utilizando componente dialog do shadcn, que abre um modal, onde foram inseridos mais componentes de tabela.

--- pizzashop-web/src/pages/app/orders/order-table-row.tsx
+++ pizzashop-web/src/pages/app/orders/order-table-row.tsx
@@ -1,28 +1,31 @@
 import { Button } from "@/components/ui/button"
+import { Dialog, DialogTrigger } from "@/components/ui/dialog"
 import { TableCell, TableRow } from "@/components/ui/table"
 import { ArrowRight, Search, X } from "lucide-react"
+import { OrderDetails } from "./order-details"
 
 export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          {/** Screen reader only, feito para o leitor de tela apenas, para quem não enxergar o ícone */}
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              {/** Screen reader only, feito para o leitor de tela apenas, para quem não enxergar o ícone */}
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">98as68a75d67q2567das</TableCell>
       <TableCell className="text-muted-foreground">
         há 15 minutos
       </TableCell>
       <TableCell>
-        <div className="flex items-center gap-2">
-          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-          <span className="font-medium text-muted-foreground">
-            Pendente
-          </span>
-        </div>
+
       </TableCell>
       <TableCell className="font-medium">
         Lucas Galarraga Burch
